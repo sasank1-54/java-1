fa55f8326a0463736a335a0001e5dcac86191295
sasank1-54 <114579796+sasank1-54@users.noreply.github.com>
2022-10-03 22:36:36 +0530

    Add files via upload

---
+++ months.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Months {
+    Scanner sc = new Scanner(System.in);
+
+    public void DaysToMonths() {
+        System.out.println("Enter number of days: ");
+        int dayNum = sc.nextInt();
+        int days = dayNum % 30;
+        int months = dayNum / 30;
+        System.out.println(dayNum + " days is equal to " + months + " Month and " + days + " days");
+    }
+
+    public static void main(String[] args) {
+
+        Months obj = new Months();
+        obj.DaysToMonths();
+    }
+}
